c947dbda434df49e4b19badb93cfde9b78a17638
devonChurch <notifydevon@gmail.com>
2019-10-05 16:44:45 +1300

    feat: 🎸 add stdout introduction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { resolve } = require("path");
+const chalk = require("chalk");
 
 const hasStartingSlash = value => value[0] === "/";
 const hasEndingSlash = value => value.substr(-1) === "/";
@@ -19,7 +20,9 @@ const sanatiseRoutes = values =>
     .map(value => addStartingSlash(removeEndingSlash(value.trim())));
 const sanatiseOutput = value => resolve(__dirname, removeEndingSlash(value));
 
-const { argv } = require("yargs")
+const {
+  argv: { root, routes, output }
+} = require("yargs")
   .example(
     'watermellon --root http://localhost:4200 --routes "/,/login,/register" --output ./dist'
   )
@@ -44,8 +47,41 @@ const { argv } = require("yargs")
     output: sanatiseOutput
   });
 
-console.log(
-  argv.root,
-  argv.routes.split(",").map(route => route.trim()),
-  argv.output
-);
+const PINK = "#ff7bb1";
+const MINT = "#cefbb7";
+const EMOJI = "🍉";
+const BULLET = "  • ";
+const SPACER = " ";
+
+const logo = chalk.hex(PINK).bgHex(MINT).bold(`
+                 _                          _              
+  __ __ __ __ _ | |_  ___  _ _  _ __   ___ | | ___  _ _    
+  \\ V  V // _' ||  _|/ -_)| '_|| '  \\ / -_)| |/ _ \\| ' \\   
+   \\_/\\_/ \\__,_| \\__|\\___||_|  |_|_|_|\\___||_|\\___/|_||_|  
+                                                           
+`);
+
+const createTitle = value => chalk.hex(PINK)(`${EMOJI} ${value}:`);
+const createArg = values =>
+  chalk.hex(MINT)(
+    values.reduce(
+      (acc, value) =>
+        // prettier-ignore
+        `${acc}
+${BULLET}${encodeURI(value)}`,
+      ""
+    )
+  );
+
+console.log(SPACER);
+console.log(logo);
+console.log(SPACER);
+console.log(createTitle("Root"));
+console.log(createArg([root]));
+console.log(SPACER);
+console.log(createTitle("Routes"));
+console.log(createArg(routes));
+console.log(SPACER);
+console.log(createTitle("Output"));
+console.log(createArg([output]));
+console.log(SPACER);
